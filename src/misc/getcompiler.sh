a99da96d3b891127f8b5e6561b1d884d67d85809
leon-v <leon@valkenb.org>
2017-06-21 17:47:57 +1200

    Added more stuff to script that prepares the build environment

--- src/misc/getcompiler.sh
+++ src/misc/getcompiler.sh
@@ -2,6 +2,9 @@
 sudo mkdir /opt/Espressif
 sudo chmod 777 /opt/Espressif
 cd /opt/Espressif
+
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install git autoconf build-essential gperf bison flex texinfo libtool libncurses5-dev wget gawk libc6-dev-i386 python-serial libexpat-dev build-essential help2man libtool-bin python python-dev -y
 
 git clone https://github.com/espressif/crosstool-NG.git
